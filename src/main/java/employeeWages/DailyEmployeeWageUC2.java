64ba6a2b5115d843f3f3a9f9509f7f6327845303
unknown <mohit16gaikwad@gmail.com>
2022-03-24 15:57:18 +0530

    UC2 added

---
+++ src/main/java/employeeWages/DailyEmployeeWageUC2.java
@@ -0,0 +1,30 @@
+package employeeWages;
+
+public class DailyEmployeeWageUC2 {
+	
+		public static void main(String[] args) {
+			
+			int fulltime=1;
+			int ERPH =20;
+			int EH=0;
+			int EW=0;
+			
+			double empcheck =Math.floor(Math.random()*10)%2;
+		
+			if(empcheck==fulltime)
+			{
+				EH=8;
+				
+			}
+			else
+			{
+				EH=0;
+				
+			}
+			EW=EH*ERPH;
+			System.out.println("Employee wage "+ EW);
+		}
+
+	}
+
+
