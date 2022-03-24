b3de7e8a5bc43574c84b06917268d9f956a5de7d
unknown <mohit16gaikwad@gmail.com>
2022-03-24 15:49:37 +0530

    Welcome Emp wages added

---
+++ src/main/java/employeeWages/WelcomeEmployeeWages.java
@@ -0,0 +1,10 @@
+package employeeWages;
+
+public class WelcomeEmployeeWages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to employee wages");
+	}
+
+}
